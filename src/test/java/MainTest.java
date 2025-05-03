05903022c00b63466404e8349c3f8f4820bfd6c4
Deniz Acay <me@denizacay.com>
2025-05-03 13:22:33 +0300

    Update MainTest.java for minor problems

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -136,7 +136,7 @@ public class MainTest {
     public void testGasPoweredCar() throws NoSuchMethodException {
         assertThat(gasPoweredCar.getName(), instanceOf(String.class));
         assertThat(gasPoweredCar.getDescription(), instanceOf(String.class));
-        assertThat(gasPoweredCar.getAverageKmPerLiter(), instanceOf(Double.class));
+        assertThat(gasPoweredCar.getAvgKmPerLitre(), instanceOf(Double.class));
         assertThat(gasPoweredCar.getCylinders(), instanceOf(Integer.class));
     }
 
@@ -146,7 +146,7 @@ public class MainTest {
         assertThat(hybridCar.getName(), instanceOf(String.class));
         assertThat(hybridCar.getDescription(), instanceOf(String.class));
         assertThat(hybridCar.getCylinders(), instanceOf(Integer.class));
-        assertThat(hybridCar.getAvgKmPerLiter(), instanceOf(Double.class));
+        assertThat(hybridCar.getAvgKmPerLitre(), instanceOf(Double.class))
         assertThat(hybridCar.getBatterySize(), instanceOf(Integer.class));
     }
 
